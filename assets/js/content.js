321d1133a89c92100cbf52a287335d66265063c8
Sam Guraya <gurayasam.1@gmail.com>
2018-12-15 09:40:25 -0500

    update

--- assets/js/content.js
+++ assets/js/content.js
@@ -32,37 +32,3 @@ window.addEventListener("mouseup", function wordSelected() {
 		chrome.runtime.onMessage.addListener(gotMessage);  // Listens for button to be pressed 
 	}
 });
-
-
-
-// function wordSelected() {
-// 	var text = "";
-
-// 	if (window.getSelection) {
-// 		text = window.getSelection().toString();
-// 		if(text === "") {
-// 			return; 
-// 		}
-// 	} 
-// 	else if (document.selection && document.selection.type != "Control") {
-// 		text = document.selection.createRange().text;
-// 	}
-// 	var x =	window.getSelection().getRangeAt(0).surroundContents(highlightElement);
-// 	console.log(x);
-
-// 	console.log(text);
-// }
-
-// function wordSelected() {
-// 	var text = window.getSelection().toString();
-// 	if(text === "") {
-// 		return; 
-// 	}
-// 	console.log(text); 
-// 	var selection= window.getSelection().getRangeAt(0);
-// 	var selectedText = selection.extractContents();
-// 	var span= document.createElement("span");
-// 	span.style.backgroundColor = "yellow";
-// 	span.appendChild(selectedText);
-// 	selection.insertNode(span);
-// }
